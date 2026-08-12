b1845f2c3c48fba90f4e078919de9ca07e8cdffa
jk <davidlara@aguida.com.mx>
2026-08-12 16:37:16 -0600

    refactor: Reorganize initialization sequence in index.php for improved readability

--- public/index.php
+++ public/index.php
@@ -8,21 +8,22 @@ use App\Shared\Infrastructure\Router\Router;
 use App\Shared\Infrastructure\Middleware\CORSMiddleware;
 use App\Shared\Infrastructure\Http\HandlerExceptions;
 
-EnvironmentLoader::load();
-$container = ContainerConfig::create();
-CORSMiddleware::handle();
-
-$dispatcher = $container->get("App\Shared\Application\Port\EventDispatcherInterface");
-
-$dispatcher->addListener("App\Auth\Domain\Events\UserRegistered",$container->get("App\Auth\Infrastructure\EventListener\SendEmailConfirmation"));
-$dispatcher->addListener("App\Auth\Domain\Events\PasswordRecoveryRequested",$container->get("App\Auth\Infrastructure\EventListener\SendPasswordRecoveryEmail"));
-$dispatcher->addListener("App\Auth\Domain\Events\ConfirmationTokenResent",$container->get("App\Auth\Infrastructure\EventListener\ResendEmailConfirmationToken"));
 
 $request_method = $_SERVER['REQUEST_METHOD'];
 $path = $_SERVER['PATH_INFO'] ?? $_SERVER['REQUEST_URI'];
 $path_clean = explode('?',$path)[0];
 
 try {
+    EnvironmentLoader::load();
+    $container = ContainerConfig::create();
+    CORSMiddleware::handle();
+
+    $dispatcher = $container->get("App\Shared\Application\Port\EventDispatcherInterface");
+
+    $dispatcher->addListener("App\Auth\Domain\Events\UserRegistered",$container->get("App\Auth\Infrastructure\EventListener\SendEmailConfirmation"));
+    $dispatcher->addListener("App\Auth\Domain\Events\PasswordRecoveryRequested",$container->get("App\Auth\Infrastructure\EventListener\SendPasswordRecoveryEmail"));
+    $dispatcher->addListener("App\Auth\Domain\Events\ConfirmationTokenResent",$container->get("App\Auth\Infrastructure\EventListener\ResendEmailConfirmationToken"));
+
     $controller = Router::resolve($request_method,$path_clean);
     $instance = $container->get($controller);
     $instance->execute();
