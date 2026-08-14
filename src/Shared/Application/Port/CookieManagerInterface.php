7aae6950711948e03605cdb4ba80d0da36bd6e13
JK <josesitoelalto@gmail.com>
2026-08-14 00:59:32 -0600

    refactor: Simplify logout process by retrieving refresh token from cookie and update CookieManager to implement get method

--- src/Shared/Application/Port/CookieManagerInterface.php
+++ src/Shared/Application/Port/CookieManagerInterface.php
@@ -5,4 +5,5 @@ namespace App\Shared\Application\Port;
 interface CookieManagerInterface {
     public function set(string $key,string $value, array $options):void;
     public function delete(string $key):void;
+    public function get(string $key):?string;
 }
