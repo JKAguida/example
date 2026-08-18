d9bf67b13b3839330cca03ff73f2a8aef1de1026
JK <josesitoelalto@gmail.com>
2026-08-18 00:56:52 -0600

    feat: Enhance role management by adding unique constraint, and findByRoleType method

--- src/Auth/Domain/Repository/RoleRepositoryInterface.php
+++ src/Auth/Domain/Repository/RoleRepositoryInterface.php
@@ -3,10 +3,11 @@
 namespace App\Auth\Domain\Repository;
 use App\Auth\Domain\Entity\Role;
 use App\Auth\Domain\ValueObject\RoleId;
+use App\Auth\Domain\ValueObject\RoleType;
 
 
 interface RoleRepositoryInterface {
     public function save(Role $role):void;
     public function findByRoleId(RoleId $roleId):?Role;
-    public function reconstitute():Role;
+    public function findByRoleType(RoleType $roleType):?Role;
 }
--- src/Auth/Infrastructure/Persistence/RoleRepository.php
+++ src/Auth/Infrastructure/Persistence/RoleRepository.php
@@ -5,6 +5,7 @@ namespace App\Auth\Infrastructure\Persistence;
 use App\Auth\Domain\Repository\RoleRepositoryInterface;
 use App\Auth\Domain\Entity\Role;
 use App\Auth\Domain\ValueObject\RoleId;
+use App\Auth\Domain\ValueObject\RoleType;
 use PDO;
 
 
@@ -28,6 +29,22 @@ final class RoleRepository implements RoleRepositoryInterface {
             "roleId" => $roleId->value(),
         ];
         $sql = "SELECT * FROM roles WHERE roleId = :roleId LIMIT 1";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($data);
+        $role = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($role){
+            return self::reconstitute($role);
+        }else{
+            return null;
+        }
+        
+    }
+
+    public function findByRoleType(RoleType $roleType):?Role {
+        $data = [
+            "roleType" => $roleType->value,
+        ];
+        $sql = "SELECT * FROM roles WHERE roleType = :roleType LIMIT 1";
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute($data);
         $role = $stmt->fetch(PDO::FETCH_ASSOC);
